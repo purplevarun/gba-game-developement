b99d79bee3fa20bb647d8d14153de8d00335485e
purplevarun <purplevarun@gmail.com>
2021-02-19 19:36:41 +0530

    .

--- 2-controlling-sprite/main.c
+++ 2-controlling-sprite/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <gb/gb.h>
+#include "4smilers.c"
 void main () {
-    
+    int i = 0;
+    set_sprite_data(0,2,smilers);
+    set_sprite_tile(0,0);
+    move_sprite(0,100,100);
+    SHOW_SPRITES;
+    while (1) {
+        if (i>3)i=0;
+        set_sprite_tile(0,i++);
+        delay (1000);
+    }
 }
