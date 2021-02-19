18c4b51ff3bce55b4c7e20af92a711a807b5cfc4
purplevarun <purplevarun@gmail.com>
2021-02-19 19:09:27 +0530

    .

--- hello-world-game/main.c
+++ hello-world-game/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "smile.c"
 void main () {
+    int i = 0;
     printf ("hello world\n");
     printf ("varun kedia\n");
     set_sprite_data (0,2,smileFace1);
@@ -9,7 +10,8 @@ void main () {
     move_sprite(0, 88, 78);
     SHOW_SPRITES;
     while (1) {
-        printf ("hello world\n");
+        move_sprite(0,10+i,10+i);
         delay(1000);
+        i++;
     }
 }
