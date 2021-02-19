57c0339c1f2ae50fa240453a38df87b9afa28ad5
purplevarun <purplevarun@gmail.com>
2021-02-19 19:18:48 +0530

    .

--- hello-world-game/main.asm
+++ hello-world-game/main.asm
@@ -73,12 +73,20 @@ _smileFace1::
 ; code
 ;--------------------------------------------------------
 	.area _CODE
-;main.c:6: void main () {
+;main.c:8: void main () {
 ;	---------------------------------
 ; Function main
 ; ---------------------------------
 _main::
-;main.c:10: set_sprite_data (0,2,smileFace1);
+	add	sp, #-2
+;main.c:9: int i = start_X, j = start_Y;
+	ld	bc, #0x0008
+	ldhl	sp,	#0
+	ld	(hl), #0x10
+	xor	a, a
+	inc	hl
+	ld	(hl), a
+;main.c:12: set_sprite_data (0,2,smileFace1);
 	ld	hl, #_smileFace1
 	push	hl
 	ld	a, #0x02
@@ -98,11 +106,88 @@ _main::
 	ld	a, #0x10
 	ld	(hl+), a
 	ld	(hl), #0x08
-;main.c:13: SHOW_SPRITES;
+;main.c:15: SHOW_SPRITES;
 	ldh	a, (_LCDC_REG+0)
 	or	a, #0x02
 	ldh	(_LCDC_REG+0),a
-;main.c:19: }
+;main.c:16: while (1) {
+00105$:
+;main.c:17: if (i>end_X || j > end_Y){
+	ld	e, b
+	ld	d, #0x00
+	ld	a, #0xa0
+	cp	a, c
+	ld	a, #0x00
+	sbc	a, b
+	bit	7, e
+	jr	Z, 00122$
+	bit	7, d
+	jr	NZ, 00123$
+	cp	a, a
+	jr	00123$
+00122$:
+	bit	7, d
+	jr	Z, 00123$
+	scf
+00123$:
+	jr	C, 00101$
+	ldhl	sp,	#0
+	ld	a, #0x98
+	sub	a, (hl)
+	inc	hl
+	ld	a, #0x00
+	sbc	a, (hl)
+	ld	a, #0x00
+	ld	d, a
+	bit	7, (hl)
+	jr	Z, 00124$
+	bit	7, d
+	jr	NZ, 00125$
+	cp	a, a
+	jr	00125$
+00124$:
+	bit	7, d
+	jr	Z, 00125$
+	scf
+00125$:
+	jr	NC, 00102$
+00101$:
+;main.c:18: i = start_X;
+	ld	bc, #0x0008
+;main.c:19: j = start_Y;
+	ldhl	sp,	#0
+	ld	(hl), #0x10
+	xor	a, a
+	inc	hl
+	ld	(hl), a
+00102$:
+;main.c:21: move_sprite (0, i,j);
+	ldhl	sp,	#0
+	ld	d, (hl)
+	ld	e, c
+;c:/users/purpl/desktop/gbdk/include/gb/gb.h:1077: OAM_item_t * itm = &shadow_OAM[nb];
+	ld	hl, #_shadow_OAM
+;c:/users/purpl/desktop/gbdk/include/gb/gb.h:1078: itm->y=y, itm->x=x;
+	ld	a, d
+	ld	(hl+), a
+	ld	(hl), e
+;main.c:22: i+=10;
+	ld	hl, #0x000a
+	add	hl, bc
+	ld	c, l
+	ld	b, h
+;main.c:23: j+=10;
+;c
+	pop	de
+	push	de
+	ld	hl, #0x000a
+	add	hl, de
+	inc	sp
+	inc	sp
+	push	hl
+	jr	00105$
+;main.c:25: }
+	add	sp, #2
 	ret
 	.area _CODE
 	.area _CABS (ABS)
