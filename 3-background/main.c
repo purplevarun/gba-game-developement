86080dc369038910e08617c8ef5a70866a0310c7
purplevarun <purplevarun@gmail.com>
2021-02-20 19:52:35 +0530

    .

--- 3-background/main.c
+++ 3-background/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <gb/gb.h>
-
+#include "gbr_1.c"
+#include "new-bg.c"
 void main () {
-    
+    set_bkg_data(0,7,TileLabels);
+    set_bkg_tiles(0,0,40,18,bg1);
+    SHOW_BKG;
+    DISPLAY_ON;
 }
