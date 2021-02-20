abc86453643c5b45a22014b008f84bf93b7f9248
purplevarun <purplevarun@gmail.com>
2021-02-20 19:48:42 +0530

    .

---
+++ 3-background/make.bat
@@ -0,0 +1,2 @@
+c:\users\purpl\desktop\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
+c:\users\purpl\desktop\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
