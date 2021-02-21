f1c7d458995f6d48bceca2bb3830a8265a3fab9e
purplevarun <purplevarun@gmail.com>
2021-02-21 09:46:17 +0530

    .

--- 3-background/main.c
+++ 3-background/main.c
@@ -3,7 +3,7 @@
 #include "Export.c"
 #include "simplebackgroundmap.c"
 void main () {
-    set_bkg_data(0,6,backgroundtiles);
+    set_bkg_data(0,7,backgroundtiles);
     set_bkg_tiles(0,0,40,18,backgroundmap);
     SHOW_BKG;
     DISPLAY_ON;
